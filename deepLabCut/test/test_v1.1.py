e72e404244d2af2a2b929e7142ebfc2efd531e7e
heyujia <he.yujia@99cloud.net>
2020-12-14 23:08:18 +0800

    Update temp comp

--- deepLabCut/test/test_v1.1.py
+++ deepLabCut/test/test_v1.1.py
@@ -14,6 +14,37 @@ import moviepy.editor as mpe
 import cv2
 
 
+def main():
+    # 主要运行的函数
+    # part_name，需要根据 .csv 文档选择需要查看的器官
+    msl = 3
+    start_time = datetime.now()
+
+    f_style_dict = {
+        "video": ".avi",
+        "location": ".csv",
+        "figure": ".jpeg"
+    }
+
+    op_list = ["video", "location"]
+
+    axis_name = ["x", "y"]
+    part_name = [
+        "ibat",
+        "tail"
+    ]
+
+    if temperature_operator(f_style_dict, op_list, axis_name, part_name, is_out_ori_figs=False, out_op_figs=False):
+        end_time = datetime.now()
+        print(print_info(), end=" ")
+        print("Total time used: {}".format(str(end_time - start_time)[:(msl-6)]))
+        print(print_info("S"), end=" ")
+        print("All operator is done!")
+        return True
+
+    return False
+
+
 def get_time(date=False, utc=False, msl=3):
     if date:
         time_fmt = "%Y-%m-%d %H:%M:%S.%f"
@@ -215,6 +246,24 @@ def get_video_info(video):
     return v_size, v_fps, v_total_fps
 
 
+def get_color_bar_info(video, v_fps, v_total_fps, x_loc=1100, high=652, up=65, down=504):
+    # 选择中间的某帧作为获取 colorbar 的原始帧
+    item_time = int(min(v_fps * 10, v_total_fps / 2)) / v_fps
+    frame_item = video.get_frame(item_time)
+    print(print_info(), end=" ")
+    print("Use the No.{} second frame to get the ColorBar". format(item_time))
+
+    if high == 652:
+        cb_info = frame_item[up: down, x_loc, :]
+        print(print_info(), end=" ")
+        print("Get the ColorBar Information!")
+        return cb_info
+    else:
+        print(print_info("E"), end=" ")
+        print("The video size is error!")
+        return False
+
+
 def video_to_figs_sequence(video, figs_dir, f_style_dict):
     # cut the video to images sequence
     try:
@@ -232,7 +281,6 @@ def video_to_figs_sequence(video, figs_dir, f_style_dict):
 
 
 def get_pixel_value(frame, xy_list):
-
     # Attention: x represent col，y represent row
     pixel_value = [
         frame[item[1], item[0], :].tolist() for item in xy_list
@@ -241,13 +289,29 @@ def get_pixel_value(frame, xy_list):
     return pixel_value
 
 
-def rgb_to_temp(rgb_items, change_mat=[0.2989, 0.5870, 0.1140]):
+def rgb_to_gray(rgb_items, change_mat=[0.2989, 0.5870, 0.1140]):
+    # 将颜色的像素点转换为一个灰度值
     gray = np.dot(rgb_items, change_mat)
     temperature = round(1 - gray / 255, 3)
     return temperature
 
 
-def frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate=100, linewidth=2):
+def rgb_to_temp(rgb_items, cb_arr, high=40, low=24):
+    # 将颜色的像素点转换为温度
+    try:
+        # 获取欧式距离的平方
+        len_square = np.sum((cb_arr -rgb_items) ** 2, axis=1)
+        near_idx = np.argmin(len_square)
+        temperature = round((low - high) * near_idx / (len(cb_arr) - 1) + high, 2)
+    except:
+        print(print_info(), end=" ")
+        print("Can not computer the temperature!")
+
+    return temperature
+
+
+def frame_operator(frame, xy_list, pixel_value, part_name, t_dict, v_size, cb_arr, reduce_rate=100, linewidth=2):
+    # 处理单帧视频
     # frame = cv2.UMat(frame)
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -268,6 +332,7 @@ def frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate=1
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     for idx, item in enumerate(pixel_value):
+        # print(idx, item)
         cv2.putText(
             frame, part_name[idx],
             (
@@ -277,13 +342,15 @@ def frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate=1
             font, 0.5, [255, 255, 0], 2
         )
         cv2.putText(
-            frame, str(rgb_to_temp(item)),
+            # frame, str(rgb_to_gray(item)),
+            frame, str(rgb_to_temp(item, cb_arr)),
             (
                 int(v_size[0] * (rect_rate[0] * 0.5 + rect_rate[2] * 0.5)),
                 int(v_size[1] * (rate_step * (idx + 0.5) + rect_rate[1]))
             ),
             font, 0.5, [255, 255, 0], 2
         )
+        t_dict[part_name[idx]].append(rgb_to_temp(item, cb_arr))
 
     # change pixel size to plot size
     f_size = [item / reduce_rate for item in v_size]
@@ -292,7 +359,7 @@ def frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate=1
         cv2.circle(frame, tuple(item), int(min(f_size) * linewidth - 1), [0, 0, 0], linewidth)
         cv2.putText(frame, part_name[idx], tuple(item), font, 0.5, [0, 0, 0], linewidth)
 
-    return frame
+    return frame, t_dict
 
 
 def time_format(m):
@@ -321,7 +388,7 @@ def op_fig_name(f_index, pre_name="labeled_frame", frame_style=".jpeg", op_fps=2
 
 
 def get_all_frame(
-        input_list, video, v_total_fps, op_df, output_dir, v_size, part_name,
+        input_list, video, v_total_fps, op_df, output_dir, v_size, part_name, cb_arr,
         op_fps=20, v_vs_loc=True, out_op_figs=False, reduce_rate=100
 ):
 
@@ -367,13 +434,23 @@ def get_all_frame(
 
     op_frames_list = list()
 
+    # 创建一个温度字典
+    temp_dict = dict()
+    for part_item in part_name:
+        temp_dict[part_item] = list()
+
     if not out_op_figs:
         for idx in range(op_len):
             frame = video.get_frame(frame_fps_to_time(idx, op_fps))
             xy_list = op_df.values[idx].reshape(-1, 2)
             pixel_value = get_pixel_value(frame, xy_list)
-            op_frame = frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate)
+            op_frame, temp_dict = frame_operator(
+                frame, xy_list, pixel_value, part_name, temp_dict, v_size, cb_arr, reduce_rate
+            )
             op_frames_list.append(op_frame)
+            if (idx + 1) % (op_fps * 10) == 0:
+                print(print_info(), end=" ")
+                print("{}/{} frames done!".format(idx + 1, op_len))
 
         print(print_info(), end=" ")
         print("All {} frames sequence is operated.".format(len(op_frames_list)))
@@ -384,19 +461,24 @@ def get_all_frame(
             frame = cv2.cvtColor(video.get_frame(frame_fps_to_time(idx, op_fps)), cv2.COLOR_BGR2RGB)
             xy_list = op_df.values[idx].reshape(-1, 2)
             pixel_value = get_pixel_value(frame, xy_list)
-            op_frame = frame_operator(frame, xy_list, pixel_value, part_name, v_size, reduce_rate)
+            op_frame, temp_dict = frame_operator(
+                frame, xy_list, pixel_value, part_name, temp_dict, v_size, cb_arr, reduce_rate
+            )
             cv2.imwrite(
                 os.path.join(output_dir, op_fig_name(idx, op_fps=op_fps)),
                 op_frame
             )
             op_frames_list.append(op_frame)
+            if (idx + 1) % (op_fps * 10) == 0:
+                print(print_info(), end=" ")
+                print("{}/{} frames done!".format(idx + 1, op_len))
 
         print(print_info(), end=" ")
         print("All {} frames sequence is operated.".format(len(op_frames_list)))
         print(print_info(), end=" ")
         print("The operated frames sequence is saved to '{}'.".format(output_dir))
 
-    return op_frames_list
+    return op_frames_list, temp_dict
 
 
 def frame_fps_to_time(idx, fps, length=3):
@@ -422,10 +504,10 @@ def labeled_video_output(op_figs_list, v_fps, output_dir, output_file_name="outp
         print("Compositing video file from operated frames sequence...")
         clip.write_videofile(os.path.join(output_dir, output_file_name + ".mp4"), fps=v_fps, audio=False)
         print(print_info(), end=" ")
-        print("Video file compositted at '{}'!".format(os.path.join(output_dir, output_file_name + ".mp4")))
+        print("Video file was composited at '{}'!".format(os.path.join(output_dir, output_file_name + ".mp4")))
     except:
         print(print_info("E"), end=" ")
-        print("Cloud not compositing video file. Please check again!")
+        print("Cloud not composite video file. Please check again!")
         return False
     return True
 
@@ -465,6 +547,11 @@ def temperature_operator(
     else:
         v_size, v_fps, v_total_fps = get_video_info(video)
 
+    cb_info = get_color_bar_info(video, v_fps, v_total_fps, high=v_size[-1])
+
+    if type(cb_info) is bool:
+        return cb_info
+
     if is_out_ori_figs:
         if not video_to_figs_sequence(video, figs_dir, f_style_dict):
             return False
@@ -472,49 +559,30 @@ def temperature_operator(
     # output
     figs_list = sorted(find_file_list(figs_dir, "*" + f_style_dict["figure"]))
 
-    op_frames_list = get_all_frame(
-        figs_list, video, v_total_fps, loc_df, op_figs_dir, v_size, part_name, v_fps, True, out_op_figs
+    op_frames_list, temp_dict = get_all_frame(
+        figs_list, video, v_total_fps, loc_df, op_figs_dir, v_size, part_name, cb_info, v_fps, True, out_op_figs
     )
 
     if not op_frames_list:
         return False
 
+    if not temperature_csv_output(temp_dict, output_dir):
+        return False
+
     return labeled_video_output(op_frames_list, v_fps, output_dir)
 
 
-def main():
-    msl = 3
-    start_time = datetime.now()
-
-    f_style_dict = {
-        "video": ".mp4",
-        "location": ".csv",
-        "figure": ".jpeg"
-    }
-
-    op_list = ["video", "location"]
-
-    axis_name = ["x", "y"]
-    # part_name = [
-    #     "bodyW",
-    #     "tailbaseW"
-    # ]
-    part_name = [
-        "tail",
-        "butt"
-    ]
-
-    if temperature_operator(f_style_dict, op_list, axis_name, part_name, is_out_ori_figs=False, out_op_figs=False):
-        end_time = datetime.now()
+def temperature_csv_output(t_dict, output_dir, output_file_name="output"):
+    try:
+        temp_df = pd.DataFrame(data=t_dict)
+        temp_df.to_csv(os.path.join(output_dir, output_file_name + ".csv"))
         print(print_info(), end=" ")
-        print("Total time used: {}".format(str(end_time - start_time)[:(msl-6)]))
-        print(print_info("S"), end=" ")
-        print("All operator is done!")
-
+        print("Successful save the csv file")
         return True
-
-    return False
-
+    except:
+        print(print_info("E"), end=" ")
+        print("Can not save the temperature csv file.")
+        return False
 
 if __name__ == '__main__':
     judge = main()
